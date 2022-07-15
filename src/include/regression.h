3719d1155808704b432ffb6cd32e14f9a5c8ddf7
Esma <esmazuurbier12@gmail.com>
2022-07-15 23:10:18 +0200

    added templates

--- src/include/regression.h
+++ src/include/regression.h
@@ -14,24 +14,59 @@
 
 /**
  * @brief Multiplies two vectors and then calculates the sum of the multiplied values.
- * a and b must be the same size.
+ * vector a and b must be the same size.
  * This function helps with the calculation of the cross-deviation and deviation about x.
  * 
- * @param a vector<int>
- * @param b vector<int>
+ * @param A vector<T>
+ * @param B vector<T>
  * @see estimateCoefficients
- * @return int sum
+ * @return sum
  */
-int multiplyAndSum(std::vector<int> a, std::vector<int> b);
+template <typename T, typename X>
+X multiplyAndSum(std::vector<T> A, std::vector<T> B)
+{
+    X sum;
+    std::vector<T> temp;
+    for (int i = 0; i < A.size(); i++)
+    {
+        temp.push_back(A[i]*B[i]);
+    }
+    sum = std::accumulate(temp.begin(), temp.end(), 0.0);
+
+    return sum;
+}
 
 /**
  * @brief Calculates the coefficients (slope and intercept) of the best fitting line
- * given X and Y values.
+ * given independent and dependent values.
  * 
- * @param X vector<int> X values (independent variables)
- * @param Y vector<int> Y values (dependent values)
- * @return std::vector<double> where first element is the slope (b1), second element is intercept (b0)
+ * @param A vector<T> (independent variables)
+ * @param B vector<T> (dependent values)
+ * @return vector<X> where first element is the slope (b1), second element is intercept (b0)
  */
-std::vector<double> estimateCoefficients(std::vector<int> X, std::vector<int> Y);
+template <typename T, typename X>
+std::vector< X > estimateCoefficients(std::vector<T> A, std::vector<T> B)
+{
+    // Sample size
+    int N = A.size();
+
+    // Calculate mean of X and Y
+    X meanA= std::accumulate(A.begin(), A.end(), 0.0) / A.size();
+    X meanB = std::accumulate(B.begin(), B.end(), 0.0) / B.size();
+
+    // Calculating cross-deviation and deviation about x
+    X SSxy = multiplyAndSum<T, T>(A, B) - (N * meanA * meanB);
+    X SSxx = multiplyAndSum<T, T>(A, A) - (N * meanA * meanA);
+
+    // Calculating regression coefficients
+    X slopeB1 = SSxy / SSxx;
+    X interceptB0 = meanB - (slopeB1 * meanA);
+
+    // Return vector, insert slope first and then intercept
+    std::vector< X > coef;
+    coef.push_back(slopeB1);
+    coef.push_back(interceptB0);
+    return coef;
+}
 
 #endif /*__REGRESSION__*/
