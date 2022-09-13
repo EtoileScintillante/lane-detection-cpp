141087e5cf19bb873776c58350449663bcd2b585
Esma <esmazuurbier12@gmail.com>
2022-09-13 16:39:46 +0200

    added return 1 if path to img was provided; (to avoid seg fault)

--- image.cpp
+++ image.cpp
@@ -7,6 +7,7 @@ int main(int argc, char* argv[])
     // Load source image
     if (argc != 2) {
         std::cout << "Usage: ./exe path-to-image" << std::endl;
+        return 1;
     }
     std::string path = argv[1];
     Mat source = imread(path, 1);
@@ -25,7 +26,7 @@ int main(int argc, char* argv[])
     Mat filteredIMG = filterColors(source.clone(), dayTime);
     imshow("Color filtering", filteredIMG);
     waitKey(0);
-    imwrite("colorFilter.png", filteredIMG);
+    //imwrite("colorFilter.png", filteredIMG);
     
     // Apply grayscale
     Mat gray = applyGrayscale(filteredIMG);
@@ -33,26 +34,26 @@ int main(int argc, char* argv[])
     // Blur image
     Mat gBlur = applyGaussianBlur(gray);
     imshow("Grayscale and blur", gBlur);
-    imwrite("grayBlur.png", gBlur);
+    //imwrite("grayBlur.png", gBlur);
 
     // Detect edges
     Mat edges = applyCanny(gray);
     imshow("Canny edge detection", edges);
     waitKey(0);
-    imwrite("canny.png", edges);
+    //imwrite("canny.png", edges);
 
     // Apply mask on canny
     Mat maskedIMG = RegionOfInterest(edges);
     imshow("Region of Interest on canny", maskedIMG);
     waitKey(0);
-    imwrite("maskedCanny.png", maskedIMG);
+    //imwrite("maskedCanny.png", maskedIMG);
 
     // Detect lines and draw lanes if possible
     std::vector<Vec4i> linesP = houghLines(maskedIMG, source.clone(), false);
     Mat lanes = drawLanes(source.clone(), linesP);
     imshow("Lanes", lanes);
     waitKey(0);
-    imwrite("result.png", lanes);
+    //imwrite("result.png", lanes);
     
     return 0;
 } 
